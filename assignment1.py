94b93c373afa85a48621833dd8c86a36fa883db7
JarrodE <jarrod.eaton@my.jcu.edu.au>
2023-10-23 10:20:30 +1000

    Add menu

--- assignment1.py
+++ assignment1.py
@@ -4,10 +4,17 @@ Date started: 21/10/2023
 GitHub URL: https://github.com/cp1404-students/a1-JarrodE
 """
 
+MENU = """Menu:
+D - Display songs
+A - Add new songs
+C - Complete a song
+Q - Quit"""
 
 def main():
-    """..."""
-    print("Song List 1.0 - by Your Name")
+    """Program that allows the user to track songs that they wish to
+    learn and songs they have completed learning"""
+    print("Song List 1.0 - by Jarrod Eaton")
+    print(MENU)
 
 
 if __name__ == '__main__':
