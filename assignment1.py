23a6d430f9ddeb8fabfa1fb12cfef48c019d6752
JarrodE <jarrod.eaton@my.jcu.edu.au>
2023-10-23 10:28:39 +1000

    Add read file function and update menu

--- assignment1.py
+++ assignment1.py
@@ -4,6 +4,7 @@ Date started: 21/10/2023
 GitHub URL: https://github.com/cp1404-students/a1-JarrodE
 """
 
+FILENAME = "songs.csv"
 MENU = """Menu:
 D - Display songs
 A - Add new songs
@@ -15,6 +16,27 @@ def main():
     learn and songs they have completed learning"""
     print("Song List 1.0 - by Jarrod Eaton")
     print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "D":
+            read_file()
+        elif choice == "A":
+            print(f"")
+        elif choice == "C":
+            print()
+        else:
+            print("Invalid menu choice")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Finished")
+    read_file()
+
+
+def read_file():
+    in_file = open(FILENAME)
+    text = in_file.read()
+    in_file.close()
+    print(text)
 
 
 if __name__ == '__main__':
